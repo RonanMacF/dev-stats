76a90a6d303f0cf9d1e576357432e313122d49f2
RonanMacF <macfhlar@tcd.ie>
2018-07-22 16:08:28 +0100

    Inital setup complete

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <div className="navbar-brand">Developer Stats</div>
+        </div>
+      </nav>
+    </div>
+  );
+};
